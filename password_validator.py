6b83923a5b0439d6736b1b5382a94e8939e1092d
Ido <ido.had7@gmail.com>
2022-07-10 17:07:12 +0300

    added code and documentation

--- password_validator.py
+++ password_validator.py
@@ -6,26 +6,52 @@ def main():
 
     if len(sys.argv)==2:
         validate(sys.argv[1])
-    else:
-        output("Please enter one password for validation ",False)
+    else: #missing or too many arguments in command line
+        output("Please enter one and only one password for validation  ",False)
 
 def validate(password):
-    if len(password)>=10:
-        hasnum=False
-        hasUpper=False
-        haslower=False
+    """
+    validating password and assuring its standards
+    :param password: the password to be validated
+    """
+    if len(password)>=10: #checking length
+        has_num=False
+        has_upper=False
+        has_lower=False
         for c in sys.argv[1]:
-            if c.isnumeric():
-                hasnum=True
+            if c.isnumeric(): #checking it contains at least one digit
+                has_num=True
+            if c.islower():#checking it contains at least one lower case char
+                has_lower=True
+            if c.isupper():#checking it contains at least one upper case char
+                has_upper=True
+            if has_lower and has_num and has_upper: #All ok, no need to check the rest
+                 output("your password meets all requirements  ",True)
+        if not has_num:
+            output("password must contain a digit",False)
+        if not has_upper:
+            output("password must contain upper case letter",False)
+        if not has_lower:
+            output("password must contain lower case letter",False)
+
+
     else:
         output("password length must be at least 10 characters",False)
 
 
 def output(text,status):
+    """
+    printing password status after validating
+    :param text: message to the user regarding password acceptance
+    :param status: False for non legit password.true for valid and accepted password
+    False will lead to Red foreground color true for green
+    """
     if status:
         print(Fore.GREEN +text)
+        sys.exit(0)
     else:
         print(Fore.RED +text)
+        sys.exit(1)
 
 
 main()
