c2c852933b26f4c0af5cc0d39fb8a9ba43f1ab64
Ido <ido.had7@gmail.com>
2022-07-14 14:17:25 +0300

    adding hw assignments no1-2 files

---
+++ hw1.1.py
@@ -0,0 +1,6 @@
+__author__ = 'Ido'
+lst=[3,7,2]
+lst2=["*"*item for item in lst  ]
+
+print(lst2)
+
---
+++ hw1.2.py
@@ -0,0 +1,9 @@
+
+
+lst1=["a","b","c","d"]
+lst2=["x","y","z","w"]
+lst_res=[]
+for i in range(1,len(lst1)+1):
+    lst_res.append(lst1[i-1]+lst2[-i])
+
+print(lst_res)
